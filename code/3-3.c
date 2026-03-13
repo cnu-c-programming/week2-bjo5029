42923449125c2c740191da439e179bd99c6cd402
Jeongeun Baek <jeongeunbaek5029@gmail.com>
2026-03-13 12:54:10 +0900

    week2

--- code/3-3.c
+++ code/3-3.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
 
 int main() {
-  unsigned int x = 0x12345678;
-
-  unsigned char *addr = (unsigned char *)&x;
-
-  printf("%x\n", *addr);
-  printf("%x\n", *(addr + 1));
-  printf("%x\n", *(addr + 2));
-  printf("%x\n", *(addr + 3));
+  printf("%f\n", 5 / 3);
+  printf("%f\n", (float)5 / 3);
 
   return 0;
 }
--- code/3-4.c
+++ code/3-4.c
@@ -9,7 +9,7 @@ int main() {
   a = 2 + 3 * 4;
   printf("%d %d %d\n", a, b, c);
 
-  a++ + ++b;
+  c = a++ + ++b;
   printf("%d %d %d\n", a, b, c);
 
   return 0;
